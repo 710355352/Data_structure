226ac26c33f15b5547d368254de8962eefecef61
Sora <710355352@qq.com>
2022-08-30 16:21:56 +0800

    重载不会，但是实现了冒泡排序

--- 排序算法/Bubble_Sort.py
+++ 排序算法/Bubble_Sort.py
@@ -1,18 +1,37 @@
-from typing import List
-
-
+from typing import List, overload
 import random
-def bubble_sort(line:List) -> List:
+
+
+
+
+@overload
+def bubble_sort(line:List)->List: ...
+@overload
+def bubble_sort(line:List,flag:bool)->List: ...
+
+def bubble_sort(line:List,arg = None) -> List:
     n = len(line)
-    for i in range(n):
-        for j in range(n-i):
-            if line[j] <= line[j+1]:
-                continue
-            else:
-                line[j],line[j+1] = line[j+1], line[j]
+    if arg:
+        for i in range(1,n):
+            flag = arg
+            for j in range(n-i):
+                if line[j] <= line[j+1]:
+                    continue
+                else:
+                    line[j],line[j+1] = line[j+1], line[j]
+                    flag = False
+
+            if flag == True:
+                break
+    else:
+        for i in range(1,n):
+            flag = arg
+            for j in range(n-i):
+                if line[j] <= line[j+1]:
+                    continue
+                else:
+                    line[j],line[j+1] = line[j+1], line[j]
     return line 
-
-
 
 if __name__ == '__main__':
     line = [random.randint(0,20) for i in range(20)]
