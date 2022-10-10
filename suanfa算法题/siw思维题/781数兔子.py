95d2882fde55a12e0409732706dc58c5892fee86
Sora <710355352@qq.com>
2022-10-10 17:59:19 +0800

    1

--- suanfa算法题/siw思维题/781数兔子.py
+++ suanfa算法题/siw思维题/781数兔子.py
@@ -5,8 +5,9 @@
 链接：https://leetcode.cn/problems/rabbits-in-forest
 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 '''
+
 class Solution:
-    def numRabbits(self, answers: List[int]) -> int:
+    def numRabbits(self, answers) -> int:
         dic = {}
         ans = 0
         for i in answers:
